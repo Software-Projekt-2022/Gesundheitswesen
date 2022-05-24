2462b928cb44eba6fff7934d6f23e799052edcc6
Malte Kanders <92604963+BTBlueTiger@users.noreply.github.com>
2022-05-24 07:32:20 +0200

    Pfad der Api zu dem vorraussichtlichen Api Pfad geändert

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const host = process.env.REACT_APP_HOST
 
-const url = 'http://localhost:5000/category'
+//const url = 'http://localhost:5000/category'
+const url = 'http://gesundheitswesen_ba:5000/category'
 
 export const fetchCategorys = () => axios.get(url);
 export const createCategory = (category) => axios.post(url, category);
