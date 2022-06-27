099b0200a193e6866cb07e5e46bf9504367b5567
Malte Kanders <92604963+BTBlueTiger@users.noreply.github.com>
2022-06-27 23:42:55 +0200

    Implementation of eventservice

--- client/src/events/EventService.js
+++ client/src/events/EventService.js
@@ -1,9 +1,46 @@
 const amqp = require('amqplib/callback_api');
 
 
-const EventService = () => {
+const microservice_queue = 'microservice.gesundheit';
 
-    console.log(amqp)
+let amqp_channel = null;
+
+const connect = () => {
+
+    amqp.connect("amqp://rabbitmq:5672", (err, connection) => {
+        if(err) throw err
+
+        connection.on('close', () => {
+            console.log("RabbitMQ connection closed");
+            connection = null
+        })
+
+        connection.createChannel((ch_err, ch) =>{
+            if(ch_err){
+                connection.close();
+                connection = null;
+                err(ch_err)
+                return
+            }
+
+            amqp_channel = ch;
+        })
+    })
 }
 
-export default EventService
+const onEventRecieved = (event) => console.log('Event recieved %s: %s',event.event_type, JSON.stringify(event));
+
+const listenForEvents = (callback) => {
+    amqp_channel.prefetch(1);
+    amqp_channel.consume(microservice_queue, (msg) => {
+        if(msg.content){
+            onEventRecieved(msg.content.toString);
+            callback(JSON.parse(msg.content.toString()));
+        }
+    }, {noAck: true}
+    )
+}
+
+connect(() => {
+    listenForEvents();
+})
